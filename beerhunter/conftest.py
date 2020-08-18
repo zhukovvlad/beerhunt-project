dc8f2195d3017f7b620039171c8ae78f8a3e2f2e
Vladimir Zhukov <zhukovvlad@yandex.ru>
2020-08-18 14:05:51 +0300

    Initial commit

---
+++ beerhunter/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from beerhunter.users.models import User
+from beerhunter.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
