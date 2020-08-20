11f2cb53cc15d7f64f434a30fe8b57567a95214d
Vladimir Zhukov <zhukovvlad@yandex.ru>
2020-08-20 15:31:44 +0300

    Modified brewery model

--- beerhunter/breweries/admin.py
+++ beerhunter/breweries/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Brewery
+
+
+@admin.register(Brewery)
+class BreweryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug')
--- beerhunter/breweries/models.py
+++ beerhunter/breweries/models.py
@@ -1,8 +1,21 @@
 from django.db import models
 
+from autoslug import AutoSlugField
+from model_utils.models import TimeStampedModel
 
-class Brewery(models.Model):
-    title = models.CharField(max_length=255)
+
+class Brewery(TimeStampedModel):
+    title = models.CharField('Title of brewery', max_length=255)
+    slug = AutoSlugField(
+        "Brewery Slug",
+        unique=True,
+        always_update=False,
+        populate_from='title'
+    )
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = "Brewery"
+        verbose_name_plural = "Breweries"
