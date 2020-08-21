107df1e3532803f15e3fe354dd853de02e61a9ea
Vladimir Zhukov <zhukovvlad@yandex.ru>
2020-08-21 12:27:39 +0300

    Added initial views to Brewery app.

--- beerhunter/breweries/views.py
+++ beerhunter/breweries/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from .models import Brewery
+
+
+class BreweryListView(ListView):
+    model = Brewery
+
+
+class BreweryDetailView(DetailView):
+    model = Brewery
