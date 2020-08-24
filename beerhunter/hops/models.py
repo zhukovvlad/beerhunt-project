b068aa8d3591334baab98a8fbfd51eafd2732b91
Vladimir Zhukov <zhukovvlad@yandex.ru>
2020-08-24 19:16:25 +0300

    Added Hop model to the hops application

--- beerhunter/hops/models.py
+++ beerhunter/hops/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from autoslug import AutoSlugField
+from model_utils.models import TimeStampedModel
+
+
+class Hop(TimeStampedModel):
+    title = models.CharField("Title of Hop", max_length=255)
+    slug = AutoSlugField(
+        "Hop Slug",
+        unique=True,
+        always_update=False,
+        populate_from='title'
+    )
+    description = models.TextField("Description", blank=True)
+
+    def __str__(self):
+        return self.title
