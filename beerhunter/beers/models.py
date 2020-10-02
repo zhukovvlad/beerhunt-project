429c40954df9924e9bff3b15abaaea841021154e
Vladimir Zhukov <zhukovvlad@yandex.ru>
2020-10-02 13:30:41 +0300

    Added images fields to Beer Model

--- beerhunter/beers/models.py
+++ beerhunter/beers/models.py
@@ -1,11 +1,24 @@
+import os
 from django.db import models
 from django.urls import reverse
+from django.utils.timezone import now as timezone_now
 
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill
 from autoslug import AutoSlugField
 from django.conf import settings
 from model_utils.models import TimeStampedModel
+from uuid import uuid4
 
 from beerhunter.breweries.models import Brewery
+
+
+def beer_directory_path_with_uuid(instance, filename):
+    now = timezone_now()
+    extension = os.path.splitext(filename)[1]
+    extension = extension.lower()
+    uuid_for_url = uuid4()
+    return f"{now:%Y/%m}/{uuid_for_url}{instance.pk}{extension}"
 
 
 class Beer(TimeStampedModel):
@@ -23,6 +36,22 @@ class Beer(TimeStampedModel):
     og = models.FloatField(null=True, blank=True)
     abv = models.FloatField(null=True, blank=True)
     ibu = models.FloatField(null=True, blank=True)
+
+    image = models.ImageField(upload_to=beer_directory_path_with_uuid)
+
+    image_icon = ImageSpecField(
+        source='image',
+        processors=[ResizeToFill(100, 100)],
+        format='PNG',
+        options={'quality': 60}
+    )
+
+    image_detail = ImageSpecField(
+        source='image',
+        processors=[ResizeToFill(150, 150)],
+        format='PNG',
+        options={'quality': 70}
+    )
 
     hunter = models.ForeignKey(
         settings.AUTH_USER_MODEL,
