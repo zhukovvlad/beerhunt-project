60491cd2e841269dca7520b1c495620899bb24ce
Vladimir Zhukov <zhukovvlad@yandex.ru>
2020-10-31 01:56:29 +0300

    Some changes for brewery list template.

--- beerhunter/breweries/views.py
+++ beerhunter/breweries/views.py
@@ -1,14 +1,30 @@
 from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+from django.db.models.aggregates import Sum, Max
+
 from .models import Brewery
 from beerhunter.beers.models import Beer
 
 
 class BreweryListView(ListView):
     model = Brewery
+    queryset = Brewery.objects.annotate(rating=Sum('brewered__vote__value'))
 
     context_object_name = "brewery_list"
+
+    #   def get_context_data(self, **kwargs):
+    #       ctx = super().get_context_data(**kwargs)
+    #       #   all_beers = Beer.objects.values('brewery__name').annotate(total_rating=Sum('vote__value'))
+    #       # ctx['total_rating'] = Beer.objects.values('brewery__name').annotate(total_rating=Sum('vote__value'))
+    #       # brewery_with_rating = Brewery.objects.values('title').annotate(most_rated=Max('brewered__vote__value'))
+    #       brewery_with_rating = Brewery.objects.all().annotate(
+    #           most_rated=Beer.objects.all_with_related_instances_and_score().order_by('-score')
+    #          )
+    #       rating = Beer.objects.all_with_related_instances_and_score().order_by('-score')[0]
+    #       print(f'We have for all beers {brewery_with_rating}')
+    #       # print(f'Rating for {brewery_with_rating[5]} is {brewery_with_rating[5]}')
+    #       return ctx
 
 
 class BreweryDetailView(DetailView):
